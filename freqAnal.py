c6ce24d4e510612cd991f8b51e407986f4f837ae
tysonggraham <tyson.g.graham@gmail.com>
2016-01-14 18:02:42 -0700

    yeah

--- freqAnal.py
+++ freqAnal.py
@@ -6,11 +6,11 @@ import numpy as np
 import math
 
 
-chunk = 1024
+chunk = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 20
+RECORD_SECONDS = 5
 
 
 def Pitch(signal):
@@ -18,6 +18,7 @@ def Pitch(signal):
     crossing = [math.copysign(1.0, s) for s in signal]
     index = find(np.diff(crossing));
     f0=round(len(index) *RATE /(2*np.prod(len(signal))))
+    f0=round(f0/50) * 50
     return f0;
 
 
@@ -33,4 +34,6 @@ frames_per_buffer = chunk)
 for i in range(0, int(RATE / chunk * RECORD_SECONDS)):
     data = stream.read(chunk)
     Frequency=Pitch(data)
+
     print ("Frequency" + str(Frequency))
+print (int(RATE / chunk * RECORD_SECONDS))
