31a3b59e4f1535c019108778770c74206b7f65df
tysonggraham <tyson.g.graham@gmail.com>
2016-01-25 14:42:48 -0700

    added comments to more variables. Should be clearer now

--- monoWav.py
+++ monoWav.py
@@ -6,16 +6,18 @@ import sys
 
 
 fname = "WaveTest.wav"; # Filename
-frate = 5000.0; # framerate as a float (also referred to as frequency rate/sample rate)
-data_size = int(frate); # integer of frate
+frate = 5000.0; # framerate as a float (also referred to as frequency rate or sample rate)
+data_size = int(frate); # integer of frate data_size and fequency are the same so duration of each char rep is 1 second
 amp = 20000.0;     # multiplier for amplitude
 userInput = sys.argv[1] if (len(sys.argv) > 1) else input('Please enter your message: \n');
-CHUNK = 32;
+CHUNK = 32; #this is calculated by frate/desired step_size or difference in hz from each character representation.
 step_size = frate/CHUNK;
+#uppercase characters array
 cap = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', '|', ':', '"', '<', '>', '?'];
+#lowercase characters array
 low = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', '\\', ';', '\'', ',','.', '/'];
 
-# Each character is assigned a frequency here.
+# Each character is assigned a frequency in hz here.
 enum = { 
 	'a' : 0,
 	'b' : 1,
@@ -69,19 +71,19 @@ sine_list_x = []
 durationStart = 0;
 
 for letter in userInput:
-	if letter.isupper():
-		duration = 3;
+	if letter.isupper(): #if it is an uppercase letter
+		duration = 3;    #set char reps duration to 3 secs
 
 		# Lowercase the letter
-		letter = letter.lower()
-	elif (letter in cap):
-		duration = 3;
+		letter = letter.lower() 
+	elif (letter in cap):	#or if it is in the caps array
+		duration = 3;    #set char reps duration to 3 secs
 
 		# Find where letter is in cap
 		# Set letter to that location in lower
 		letter = low[cap.index(letter)];
 	else:
-		duration = 1;
+		duration = 1;    #set char reps duration to 1 sec
 		
 	#This is to test what frequency we assigned for each character. The default case is 4500 which is for the space
 	#print (freq.get(letter, 4500))
@@ -92,12 +94,16 @@ for letter in userInput:
 	for x in range(durationStart * data_size, durationEnd * data_size):
 		sine_list_x.append(math.sin(2*math.pi*((enum.get(letter, 46) + 1) * step_size)*(x/frate)))
 	durationStart = durationEnd;
+
+#############################################################################
+# This is where we write to the file.
+#############################################################################
 wav_file = wave.open(fname, 'w')
 #This should make it mono
 nchannels = 1
-sampwidth = 2
-framerate = int(frate)
-nframes = data_size
+sampwidth = 2 #width of each sample in bytes.
+framerate = int(frate) #TODO think about deleting and replacing with data_width or frate. They all share same value
+nframes = data_size #TODO think about replacing with data_size. May keep for clarity.
 comptype = "NONE"
 compname = "not compressed"
 
@@ -111,7 +117,9 @@ for s in sine_list_x:
 wav_file.close()
 
 
-
+#############################################################################
+# This is where we play the file we just wrote.
+#############################################################################
 wf = wave.open(fname, 'rb')
 
 p = pyaudio.PyAudio()
