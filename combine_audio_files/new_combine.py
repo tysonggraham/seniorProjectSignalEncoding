f1913538a889046cd8b5f17adf39a71484301c7d
tyson <tyson.g.graham>
2016-02-18 18:34:01 -0700

    did another test

--- combine_audio_files/new_combine.py
+++ combine_audio_files/new_combine.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 
 sound1 = AudioSegment.from_wav("song.wav")
-sound2 = AudioSegment.from_wav("song2.wav")
+sound2 = AudioSegment.from_wav("sound1.wav")
 
 combined_sounds = sound1.overlay(sound2, position = 0)
 combined_sounds.export("song3.wav", format="wav")
