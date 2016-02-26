576eb7bf740fb2a11716cc9676960a8885386926
tyson <tyson.g.graham>
2016-02-25 17:46:57 -0700

    Got it printing out character representation

--- read_from_wave/readMultipleFreqsFromWav.py
+++ read_from_wave/readMultipleFreqsFromWav.py
@@ -10,59 +10,60 @@ import math
 
 # Each character is assigned a frequency in hz here.
 enum = { 
-  'a' : 0,
-  'b' : 1,
-  'c' : 2,
-  'd' : 3,
-  'e' : 4,
-  'f' : 5,
-  'g' : 6,
-  'h' : 7,
-  'i' : 8,
-  'j' : 9,
-  'k' : 10,
-  'l' : 11,
-  'm' : 12,
-  'n' : 13,
-  'o' : 14,
-  'p' : 15,
-  'q' : 16,
-  'r' : 17,
-  's' : 18,
-  't' : 19,
-  'u' : 20,
-  'v' : 21,
-  'w' : 22,
-  'x' : 23,
-  'y' : 24,
-  'z' : 25,
-  '`' : 26,
-  '1' : 27,
-  '2' : 28,
-  '3' : 29,
-  '4' : 30,
-  '5' : 31,
-  '6' : 32,
-  '7' : 33,
-  '8' : 34,
-  '9' : 35,
-  '0' : 36,
-  '-' : 37,
-  '=' : 38,
-  '[' : 39,
-  ']' : 40,
-  '\\' : 41,
-  ';' : 42,
-  '\'' : 43,
-  ',' : 44,
-  '.' : 45,
-  '/' : 46,
+  0 : 'a',
+  1 : 'b',
+  2 : 'c',
+  3 : 'd',
+  4 : 'e',
+  5 : 'f',
+  6 : 'g',
+  7 : 'h',
+  8 : 'i',
+  9 : 'j',
+  10 : 'k',
+  11 : 'l',
+  12 : 'm',
+  13 : 'n',
+  14 : 'o',
+  15 : 'p',
+  16 : 'q',
+  17 : 'r',
+  18 : 's',
+  19 : 't',
+  20 : 'u',
+  21 : 'v',
+  22 : 'w',
+  23 : 'x',
+  24 : 'y',
+  25 : 'z',
+  26 : '`',
+  27 : '1',
+  28 : '2',
+  29 : '3',
+  30 : '4',
+  31 : '5',
+  32 : '6',
+  33 : '7',
+  34 : '8',
+  35 : '9',
+  36 : '0',
+  37 : '-',
+  38 : '=',
+  39 : '[',
+  40 : ']',
+  41 : '\\',
+  42 : ';',
+  43 : '\'',
+  44 : ',',
+  45 : '.',
+  46 : '/'
 }
 
 def determineCharRep(fs, freq):
-  CHUNK = 32;
+  CHUNK = 32
   step_size = fs/CHUNK
-  print(freq/step_size)
+  letter = (freq/step_size)-1
+  return enum.get(letter)
 
 #*****************************************************************************************
 # This is our implementation of the dft
@@ -127,12 +128,16 @@ for x in range(len(splitData)):
   else:
     amplitude.append(1/N * abs(compute_fft_numpy(splitData[x])))
 #*********************Reorder the results to print out prettier**************************************
-finalFrequencies = np.fft.fftshift(freqs)
-finalAmplitudes = np.fft.fftshift(amplitude)
-#*******************************Print out the results*************************************************
-determineCharRep(8192, 2560);
+#finalFrequencies = np.fft.fftshift(freqs)
+finalFrequencies = freqs
+#finalAmplitudes = np.fft.fftshift(amplitude)
+finalAmplitudes = amplitude
+#*******************************Print out the results************************************************
+#determineCharRep(8192, 2560);
+stringOfResults = ""
 print ('freq(Hz) : amplitude')
 for i in range(len(finalAmplitudes)):
-  for x in range(len(finalAmplitudes[i])):
-    if (finalAmplitudes[i][x] > 1000 and finalFrequencies[x] >= 0) :
-      print (finalFrequencies[x] , ' : ' ,finalAmplitudes[i][x])
+  maxIndex = np.argmax(finalAmplitudes[i])
+  print( finalFrequencies[maxIndex], ':', finalAmplitudes[i][maxIndex])
+  stringOfResults += (determineCharRep(fs,abs(finalFrequencies[maxIndex])))
+print(stringOfResults)
